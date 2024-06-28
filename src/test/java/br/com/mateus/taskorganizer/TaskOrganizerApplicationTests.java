ec40d38c38cdf733e74786aa6ceedccc268fb676
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-28 16:27:06 -0300

    refactor: update `TaskOrganizerApplicationTests`

--- src/test/java/br/com/mateus/taskorganizer/TaskOrganizerApplicationTests.java
+++ src/test/java/br/com/mateus/taskorganizer/TaskOrganizerApplicationTests.java
@@ -1,13 +1,14 @@
 package br.com.mateus.taskorganizer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class TaskOrganizerApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
