bd7a82ab3ebeb4816d9143e9d25ec637b627c64c
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-28 16:26:20 -0300

    refactor: update `StatusTaskDeserializer`

--- src/main/java/br/com/mateus/taskorganizer/infra/serialization/StatusTaskDeserializer.java
+++ src/main/java/br/com/mateus/taskorganizer/infra/serialization/StatusTaskDeserializer.java
@@ -18,7 +18,12 @@ public class StatusTaskDeserializer extends StdDeserializer<StatusTask> {
 
     @Override
     public StatusTask deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-        String value = p.getText();
-        return StatusTask.fromString(value);
+        try {
+            String value = p.getText();
+            return StatusTask.fromString(value);
+
+        } catch (IOException | IllegalArgumentException ex) {
+            return null;
+        }
     }
 }
